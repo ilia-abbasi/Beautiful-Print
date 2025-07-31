67fe40ac5967fc250acdc761f0d726da41ba7534
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-07-30 20:14:06 -0700

    Added exception case for ctrl-c

--- bp.py
+++ bp.py
@@ -5,5 +5,7 @@ def main() -> None :
 if __name__ == "__main__":
     try:
         main()
+    except KeyboardInterrupt:
+        print("\nYou 'Ctrl-C'ed out of this place!")
     except Exception as e:
         print(f"Something went wrong. Info on what happened: {e}")
